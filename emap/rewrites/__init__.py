42e5eb5483dc8a29b71b8609b2681f31c7aa52ed
Sijie Kong <sijie_kong@ucsb.edu>
2025-08-10 19:43:30 +0000

    Implemented rewrite_assoc()

---
+++ emap/rewrites/__init__.py
@@ -0,0 +1,4 @@
+from .basic import (
+    rewrite_comm,
+    rewrite_assoc_to_right
+)
--- emap/rewrites/basic.py
+++ emap/rewrites/basic.py
@@ -12,8 +12,10 @@ def rewrite_comm(db: NetlistDB, target_types: list[str]) -> int:
         "SELECT type, a, b, y FROM aby_cells WHERE type IN ({})".format(",".join("?" * len(target_types))),
         target_types
     )
-    newrows = [(type_, b, a, y) for type_, a, b, y in cur]
-    cur.executemany("INSERT OR IGNORE INTO aby_cells (type, a, b, y) VALUES (?, ?, ?, ?)", newrows)
+    cur.executemany(
+        "INSERT OR IGNORE INTO aby_cells (type, a, b, y) VALUES (?, ?, ?, ?)",
+        [(type_, b, a, y) for type_, a, b, y in cur]
+    )
     db.commit()
     return cur.rowcount
 
@@ -22,7 +24,7 @@ def rewrite_assoc_to_right(db: NetlistDB, target_types: list[str]) -> int:
     """
     Rewrite associative cells to right associative form.
     E.g. (a + b) + c => a + (b + c)
-    NOTE: the width of b + c would be the same as (a + b) + c to preserve the semantics
+    NOTE: The width of b + c should be the same as (a + b) + c to preserve the semantics.
     Return the number of rows rewritten.
     """
     cur = db.execute("""
@@ -33,19 +35,22 @@ def rewrite_assoc_to_right(db: NetlistDB, target_types: list[str]) -> int:
         target_types
     )
 
-    # first, build b + c if not exists
     rows = cur.fetchall()
     newrows = []
     for type_, a, b, c, y in rows:
-        cur.execute("SELECT y from aby_cells WHERE type = ? AND a = ? AND b = ?", (type_, b, c))
-        res = cur.fetchone()
-        if res is None:   # not exists
-            b_add_c = db.next_wires(NetlistDB.width_of(y))
-            cur.execute("INSERT OR IGNORE INTO aby_cells (type, a, b, y) VALUES (?, ?, ?, ?)", (type_, b, c, b_add_c))
+        cur.execute("SELECT MAX(idx) FROM wirevec_members WHERE wirevec = ?", (y,))
+        width_b_add_c = cur.fetchone()[0] + 1
+        cur.execute(
+            "SELECT y FROM aby_cells WHERE type = ? AND a = ? AND b = ? AND (SELECT MAX(idx) + 1 FROM wirevec_members WHERE wirevec = y) = ? LIMIT 1",
+            (type_, b, c, width_b_add_c)
+        )
+        row = cur.fetchone()
+        if row is None:
+            # b + c does not exist, create it
+            newrows.append((type_, a, db._create_or_lookup_wirevec([db.auto_id for _ in range(width_b_add_c)], y)))
         else:
-            b_add_c = res[0]
-        newrows.append((type_, a, b_add_c, y))
+            # b + c exists, use it
+            newrows.append((type_, a, row[0], y))
     cur.executemany("INSERT OR IGNORE INTO aby_cells (type, a, b, y) VALUES (?, ?, ?, ?)", newrows)
     db.commit()
-
     return cur.rowcount
